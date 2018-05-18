b930cc41fd218010a9441beeb340d68f414466e3
yungwifi <smerryman@me.com>
2018-05-17 23:34:00 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :users do
+      resources :fish do
+        resources :days do
+          resources :rounds do
+          end
+        end
+      end
+    end 
+  end
 end
