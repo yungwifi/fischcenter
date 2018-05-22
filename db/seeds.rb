dff3450618326d9e11ffbdace7b479b508875ac6
yungwifi <smerryman@me.com>
2018-05-21 21:27:14 -0400

    seeded data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,29 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require_relative './fish_data.rb'
-require_relative './user_data.rb'
-
-Fish.destroy_all
 User.destroy_all
+Fish.destroy_all
 
-fish_data = get_fish_data
-user_data = get_user_data
+max = User.create!({first_name: "Maxime", last_name: "Simonet"})
+matt = User.create!({first_name: "Matt", last_name: "Harrigan"})
+choe = User.create!({first_name: "Andrew", last_name: "Choe"})
+dave = User.create!({first_name: "David", last_name: "Bonowits"})
 
-fish_data.each_pair do |user, fish|
-  info = user_data[user]
-  current_user = User.create!({
-    first_name:         info[:first_name],
-    last_name:          info[:last_name],
-    photo_url:    info[:photo_url],
-  })
-
-  fish.each do |fish|
-    Fish.create!({
-      name:        fish[:name],
-      user:       current_user
-    })
-  end
-end
+Fish.create!({name: "Tx Cupper", user_id: max.id})
+Fish.create!({name: "Th'lump", user_id: max.id})
+Fish.create!({name: "Bijou", user_id: max.id})
+Fish.create!({name: "Mom", user_id: matt.id})
+Fish.create!({name: "Mimosa", user_id: matt.id})
+Fish.create!({name: "Ale", user_id: matt.id})
+Fish.create!({name: "Jeremy Legg", user_id: choe.id})
+Fish.create!({name: "Style Toy", user_id: choe.id})
+Fish.create!({name: "Hamburger", user_id: choe.id})
+Fish.create!({name: "greenbird", user_id: dave.id})
+Fish.create!({name: "slider", user_id: dave.id})
+Fish.create!({name: "Hot Steve", user_id: dave.id})
 
 puts "Seed Success"
