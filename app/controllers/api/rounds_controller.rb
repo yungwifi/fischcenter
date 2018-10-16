1112930f4fa408c82715b72c40cb13256a73e8e4
yungwifi <smerryman@me.com>
2018-10-16 16:04:38 -0400

    added total score to fish list

--- app/controllers/api/rounds_controller.rb
+++ app/controllers/api/rounds_controller.rb
@@ -3,4 +3,20 @@ class Api::RoundsController < ApplicationController
         @round = Fish.find(params[:fish_id]).round
         render json: @round
     end
+
+    def show 
+        @round = Round.find(params[:id])
+        render json: @round
+    end
+
+    def update 
+        @round = Round.find(params[:id])
+        @round.update!(round_params)
+        render json: @round
+    end
+
+    private
+    def round_params
+        params.require(:round).permit(:round_one, :round_two, :round_three, :bonus_round)
+    end
 end
--- client/src/components/Rounds.js
+++ client/src/components/Rounds.js
@@ -4,50 +4,78 @@ import styled from 'styled-components'
 
 const RoundStyled = styled.div`
 display: flex;
-flex-direction: row;`
+flex-direction: row;
+margin-left: 10px;`
 
 class Rounds extends Component {
     state = {
-        rounds: []
+        rounds: [3, 4],
+        roundsTotal: 4
+
     }
 
     componentDidMount() {
         this.getRounds()
+        this.getRoundsTotal()
+    }
+
+    getRoundsTotal() {
+        function getSum(total, num) {
+            return total + num
+        }
+        var roundsSum = this.state.rounds.reduce(getSum)
+        this.setState({ roundsTotal: roundsSum })
+
     }
 
     getRounds = async () => {
         const fishId = this.props.fishId
-        console.log("FISH ID", fishId)
-        const res = await axios.get(`/api/users/1/fish/${fishId}/rounds`)
-        console.log("GETTING ROUND", res.data)
+        const res = await axios.get(`/api/users/2/fish/${fishId}/rounds`)
         await this.setState({ rounds: res.data })
 
     }
 
-    handleChange = (e) => {
-        const round = [...this.state.rounds]
-        console.log("ROUND CHANGE", round)
-        round[e.target.name] = e.target.value
-        console.log("HANDLE CHANGE EVENT", e.target.value)
-        this.setState({ round })
+    handleChange = (updatedRound, e) => {
+        const rounds = [...this.state.rounds]
+        const newRound = rounds.map((round) => {
+            if (round.id === updatedRound.id) {
+                rounds[e.target.name] = e.target.value
+            }
+            return rounds
+        })
+        console.log("HANDLE CHANGE", e.target.value)
+        this.setState({ rounds: newRound })
+    }
+
+    updateScore = async (roundsId) => {
+        const fishId = this.props.fishId
+        const updatedRound = this.state.rounds
+        const roundId = this.state.rounds.id
+        console.log("-UPDATED ROUND-", updatedRound, "-FISH ID-", fishId, "-ROUND ID-", roundId)
+        const res = await axios.patch(`/api/users/1/fish/${fishId}/rounds/${roundId}`, updatedRound, { roundsId })
+        console.log("POSTING NEW ROUND", res)
+        await this.setState({ rounds: res.data })
     }
 
     render() {
-        const rounds = this.state.rounds.map((round, i) => {
-            return (
-                <div key={i}>
-                    <RoundStyled >
-                        <div> Round 1 <input onChange={this.handleChange} name="round_one" type="text" value={round.round_one} /></div>
-                        <div> Round 2 <input onChange={this.handleChange} name="round_two" type="text" value={round.round_two} /> </div>
-                        <div> Round 3 <input onChange={this.handleChange} name="round_three" type="text" value={round.round_three} /></div>
-                        <div> Bonus Round <input onChange={this.handleChange} name="bonus_round" type="text" value={round.bonus_round} /></div>
-                    </RoundStyled>
-                </div>
-            )
-        })
         return (
             <div>
-                {rounds}
+                {/* {rounds} */}
+                <RoundStyled >
+                    <div> Round 1 {this.state.rounds.id} <input onChange={(e) => { this.handleChange(this.state.rounds, e) }}
+                        name="round_one" type="text" value={this.state.rounds.round_one}
+                        onBlur={() => this.updateScore(this.state.rounds.id)} /></div>
+                    <div> Round 2 <input onChange={(e) => { this.handleChange(this.state.rounds, e) }}
+                        name="round_two" type="text" value={this.state.rounds.round_two}
+                        onBlur={() => this.updateScore(this.state.rounds.id)} /> </div>
+                    <div> Round 3 <input onChange={(e) => { this.handleChange(this.state.rounds, e) }}
+                        name="round_three" type="text" value={this.state.rounds.round_three}
+                        onBlur={() => this.updateScore(this.state.rounds.id)} /></div>
+                    <div> Bonus Round <input onChange={(e) => { this.handleChange(this.state.rounds, e) }}
+                        name="bonus_round" type="text" value={this.state.rounds.bonus_round}
+                        onBlur={() => this.updateScore(this.state.rounds.id)} /></div>
+                    <div>Total: {this.state.roundsTotal}</div>
+                </RoundStyled>
             </div>
         );
     }
